f4cfe6c99da975b2ddf633cb0801254c024419ab
luckyNautalis <noahschmidt89@gmail.com>
2023-07-17 16:27:42 -0600

    Complete leetcode problem #1929

---
+++ src/leetcode-1929.java
@@ -0,0 +1,22 @@
+// Problem 1929: Concatenation of Array
+
+/**
+ * Constraints:
+ *
+ * 1) n == nums.length
+ * 2) 1 <= n <= 1000
+ * 3) 1 <= nums[i] <= 1000
+ *
+ */
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[] = new int[2 * nums.length];
+        int start = 0;
+        int mid = nums.length;
+        do {ans[start] = nums[start]; ans[mid] = nums[start]; start++; mid++;}
+        while (start < nums.length);
+
+        return ans;
+    }
+}
