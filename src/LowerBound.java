4755af73d1f0e8c577118a0775997c068c047e28
luckyNautalis <130601224+luckyNautalis@users.noreply.github.com>
2023-07-28 15:53:56 -0600

    Implement lowerBound function for finding the smallest index greater or equal to a target in a sorted int array.

--- src/LowerBound.java
+++ src/LowerBound.java
@@ -1,20 +1,29 @@
 class LowerBound {
-    // Find the index of the element that is the smallest next to the target.
-    public static int lowerBound(int[] array, int target) {
-        int low = 0, mid, high = array.length - 1;
+    /*
+     * Given some number this function will determine the lower bound, that is,
+     * the smallest index of a sorted int array where it is greater or equal
+     * to the number provided.
+     * @param array A sorted array of int.
+     * @param target The lower bound is found in relation to the target.
+     * @return low The lowest index greater or equal to the target.
+     */
+    public static int lowerBound(int[] nums, int target) {
+        int low = 0, mid, high = nums.length - 1;
         while (low <= high) {
             mid = (low + high) / 2;
-            if (array[mid] >= target) high = mid - 1;
-            else low = mid + 1;
+            if (nums[mid] >= target)
+                high = mid - 1; // Search to the left of 'mid', index.
+            else low = mid + 1; // Search to the right of 'mid', index.
         }
-        if (low < array.length && array[low] < target) low++;
+        if (low < nums.length && nums[low] < target)
+            // If the current lower bound, 'low' is less than the size of the
+            // list 'numsList' then we know the lower bound is not already the
+            // last index/element of the 'numList'.
+            // While, If the 'target' is greater than the last element of the
+            // list then lower bound is not in the list and we must increment to
+            // make the lower bound the last index/element of 'numList'.
+            low++;
 
         return low;
-    }
-
-    public static void main(String[] args) {
-
-        int[] nums = {1,2,3,4,5,6,8,33,44,47,77};
-        System.out.println(lowerBound(nums, 88));
     }
 }
