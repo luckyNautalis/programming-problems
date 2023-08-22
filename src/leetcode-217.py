3fe40648e97c496a48d0cdce490235b290ec68a1
luckyNautalis <noahschmidt89@gmail.com>
2023-08-22 09:33:01 -0600

    complete leetcode problem #217

---
+++ src/leetcode-217.py
@@ -0,0 +1,19 @@
+# Problem 217: Contains Duplicate
+
+"""
+Constraints:
+
+1) 1 <= nums.length <= 10^5
+2) -10^9 <= nums[i] <= 10^9
+
+"""
+
+class Solution:
+
+    def containsDuplicate(self, nums: list[int]) -> bool:
+
+        has_duplicate : bool = False
+        nums_set : set = set(nums)
+        if (len(nums) > len(nums_set)): has_duplicate = True
+
+        return has_duplicate
