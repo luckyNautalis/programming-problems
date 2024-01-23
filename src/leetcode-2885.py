8044edbe6e423cbcfa0d5e1ada4edce9a42e641d
luckyNautalis <noahschmidt89@gmail.com>
2024-01-22 17:51:05 -0700

    complete leetcode-2885

---
+++ src/leetcode-2885.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Problem 2885: Rename Columns
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.rename(columns={'id':'student_id', 'first':'first_name', 'last':'last_name', 'age':'age_in_years'},
+                    inplace=True)
+    return students
